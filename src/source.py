27b960f7a4b79b6bacbf55a41ecd92285372e47a
NotMyThingy <miska.janhunen@protonmail.com>
2021-04-06 12:23:08 +0300

    käyttöliittymän runko: sovellus luokka

--- src/source.py
+++ src/source.py
@@ -1,9 +1,41 @@
 class Puhelinluettelo:
     def __init__(self) -> None:
-        self.__henkilot = []
+        self.__henkilot = {}
 
     def lisaa_numero(self, nimi: str, numero: str):
-        pass
+        if nimi not in self.__henkilot:
+            # The person is associated with a list of phone numbers
+            self.__henkilot[nimi] = []
+
+        self.__henkilot[nimi].append(numero)
 
     def hae_numerot(self, nimi: str):
-        pass
+        if nimi not in self.__henkilot:
+            return None
+
+        return self.__henkilot[nimi]
+
+
+class PuhelinluetteloSovellus:
+    def __init__(self) -> None:
+        self.__luettelo = Puhelinluettelo()
+
+    def ohje(self):
+        print('komennot:')
+        print('0 - lopetus')
+
+    def suorita(self):
+        self.ohje()
+        while True:
+            print()
+            komento = input('komento: ')
+            if komento == '0':
+                break
+
+
+# testikoodia
+if __name__ == '__main__':
+    luettelo = Puhelinluettelo()
+    luettelo.lisaa_numero("Erkki", "02-123456")
+    print(luettelo.hae_numerot("Erkki"))
+    print(luettelo.hae_numerot("Emilia"))
